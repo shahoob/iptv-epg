b18c1d6375fe8e1f4bfc4f8372e7ba148944ec8e
Shehab Ali Tweel <shehabtweel@gmail.com>
2023-09-13 13:25:07 +0000

    Concurrently grab channels in one command

--- scripts/commands/epg/grab.js
+++ scripts/commands/epg/grab.js
@@ -4,7 +4,7 @@ const { EPGGrabber, generateXMLTV, Program } = require('epg-grabber')
 const { logger: _logger, date, timer, file, parser, api, zip } = require('../../core')
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
-const { Listr, ListrLogger, ListrLogLevels, ListrDefaultRendererLogLevels, color, VerboseRenderer } = require('listr2')
+const { Listr, ListrLogger, ListrLogLevels, ListrDefaultRendererLogLevels, color } = require('listr2')
 const CronJob = require('cron').CronJob
 
 dayjs.extend(utc)
@@ -13,18 +13,21 @@ const BASE_DIR = process.env.BASE_DIR || '.'
 const CURR_DATE = process.env.CURR_DATE || new Date()
 
 program
-  .requiredOption('-s, --site <name>', 'Name of the site to parse')
+  .requiredOption('-s, --site <name>', 'Name of the site to parse. Use commas (,) to separate multiple sites without a space in between')
   .option('-l, --lang <code>', 'Filter channels by language (ISO 639-2 code)')
-  .option('-o, --output <path>', 'Path to output file')
+  .option('-o, --output <path>', 'Path to output file. Use commas (,) to separate multiple paths without a space in between. Must be the same order as the site names')
   .option('--cron <expression>', 'Schedule a script run')
+  .option('-c, --concurrcncy <value>', 'Number of concurrent tasks', 3)
   .option('--gzip', 'Create a compressed version of the guide as well', false)
   .parse(process.argv)
 
 const options = program.opts()
 
-options.output = options.output || file.resolve(`${BASE_DIR}/guides/{lang}/{site}.xml`)
-options.config = file.resolve(`${BASE_DIR}/sites/${options.site}/${options.site}.config.js`)
-options.channels = file.resolve(`${BASE_DIR}/sites/${options.site}/${options.site}*.channels.xml`)
+const sites = options.site.split(',')
+options.output = (options.output?.split(',') || null) || sites.map(site => file.resolve(`${BASE_DIR}/guides/{lang}/${site}.xml`))
+options.config = sites.map(site => file.resolve(`${BASE_DIR}/sites/${site}/${site}.config.js`))
+options.channels = sites.map(site => file.resolve(`${BASE_DIR}/sites/${site}/${site}*.channels.xml`))
+// options.concurrency = options.concurrency || 3
 
 let runIndex = 0
 const logger = new ListrLogger({
@@ -49,17 +52,28 @@ async function main() {
     logger.log(ListrLogLevels.OUTPUT, `  ${prop}: ${options[prop]}`)
   }
 
-  const config = await loadConfig(options.config)
-  const queue = await createQueue(options.channels, config)
-  const outputPath = options.output
+  const configs = await Promise.all(options.config.map(config => loadConfig(config)))
+  const queues = await Promise.all(
+    options.channels.map((channel, i) => createQueue(channel, configs[i]))
+  )
+
+  const sites_queues = _.zip(configs, queues, options.output, sites)
+
+  async function runQueues(sites_queues) {
+    if (sites_queues.length > 1) {
+      await runJobs(sites_queues)
+    } else {
+      await runJob(sites_queues[0][0], sites_queues[0][1], sites_queues[0][2])
+    }
+  }
 
   if (options.cron) {
     const job = new CronJob(options.cron, function () {
-      runJob(config, queue, outputPath)
+      runQueues(sites_queues)
     })
     job.start()
   } else {
-    await runJob(config, queue, outputPath)
+    await runQueues(sites_queues)
   }
 }
 
@@ -88,28 +102,59 @@ function logConfig(config, level = 1) {
   }
 }
 
+async function runJobs(sites_queues) {
+  /** @type {Listr[]} */
+  const jobs = sites_queues.map(([config, queue, outputPath]) => createJob(queue, config, outputPath))
+
+  const listr = new Listr([
+    {
+      title: 'Batch grab sites',
+      task: (ctx, task) => task.newListr(
+        sites.map((site, i) => {
+          return {
+            title: site,
+            task: () => jobs[i]
+          }
+        })
+      )
+    }
+  ], { concurrent: 3 })
+
+  timer.start()
+  await listr.run()
+  logger.log(ListrLogLevels.COMPLETED, `  done in ${timer.format('HH[h] mm[m] ss[s]')}`)
+}
+
 async function runJob(config, queue, outputPath) {
   runIndex++
   logger.log(ListrLogLevels.STARTED, `run #${runIndex}:`)
 
-  const listr = new Listr([
-    { title: 'Grab channels', task: async (ctx, task) => {
-      task.title = 'Grabbing channels...'
-      const [channels, programs] = await grab(queue, config, task)
-      ctx.channels = channels
-      ctx.programs = programs
-    } },
-    { title: 'Save to XMLTV', task: async (ctx, task) => {
-      task.title = 'Saving to XMLTV...'
-      await save(outputPath, ctx.channels, ctx.programs, task)
-    } }
-  ], {concurrent: false})
+  const job = createJob(queue, config, outputPath)
 
   timer.start()
 
-  await listr.run()
+  await job.run()
   
   logger.log(ListrLogLevels.COMPLETED, `  done in ${timer.format('HH[h] mm[m] ss[s]')}`)
+}
+
+function createJob(queue, config, outputPath) {
+  return new Listr([
+    {
+      title: 'Grab channels', task: async (ctx, task) => {
+        task.title = 'Grabbing channels...'
+        const [channels, programs] = await grab(queue, config, task)
+        ctx.channels = channels
+        ctx.programs = programs
+      }
+    },
+    {
+      title: 'Save to XMLTV', task: async (ctx, task) => {
+        task.title = 'Saving to XMLTV...'
+        await save(outputPath, ctx.channels, ctx.programs, task)
+      }
+    }
+  ], { concurrent: false })
 }
 
 async function grab(queue, config, task) {
