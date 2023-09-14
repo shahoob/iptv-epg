b96ad9291df52157756924b71184c49e697d1bb6
Shehab Ali Tweel <shehabtweel@gmail.com>
2023-09-14 21:15:57 +0000

    fix listr2 not realising that we're still downloading stuff

--- scripts/commands/api/load.js
+++ scripts/commands/api/load.js
@@ -1,7 +1,7 @@
-const { Listr } = require('listr2')
+const { Listr, PRESET_TIMER } = require('listr2')
 const { mkdir } = require('fs/promises')
 const { existsSync, createWriteStream } = require('fs')
-const { resolve } = require('path')
+const { resolve, join } = require('path')
 const https = require('node:https')
 
 const listr = new Listr([
@@ -17,45 +17,84 @@ const listr = new Listr([
     },
     {
         title: 'Load data',
-        task: async (ctx, task) => task.newListr([
-            { title: 'Load channels', task: async () => {
-                https.get('https://iptv-org.github.io/api/channels.json', res => {
-                    res.pause()
-                    const file = createWriteStream(resolve(__dirname, '../../tmp/data/channels.json'), { flags: 'w' })
-                    res.pipe(file)
-                    res.resume()
+        task: async (ctx, _task) => _task.newListr([
+            { title: 'Load channels', task: (ctx, task) => {
+                return new Promise((resolve, reject) => {
+                    https.get('https://iptv-org.github.io/api/channels.json', res => {
+                        res.pause()
+                        res.once('close', resolve)
+                        const file = createWriteStream(join(ctx.path, 'channels.json'), { flags: 'w' })
+                        let downloaded = 0
+                        let total = parseInt(res.headers['content-length'], 10)
+                        res.on('data', (chunk) => {
+                            downloaded += chunk.length
+                            task.output = `${(downloaded / total * 100).toFixed(2)}%`
+                            file.write(chunk)
+                        })
+                        res.resume()
+                    })
                 })
             } },
-            { title: 'Load countries', task: async () => {
-                https.get('https://iptv-org.github.io/api/countries.json', res => {
-                    res.pause()
-                    const file = createWriteStream(resolve(__dirname, '../../tmp/data/countries.json'), { flags: 'w' })
-                    res.pipe(file)
-                    res.resume()
+            { title: 'Load countries', task: (ctx, task) => {
+                return new Promise((resolve, reject) => {
+                    https.get('https://iptv-org.github.io/api/countries.json', res => {
+                        res.pause()
+                        res.once('close', resolve)
+                        const file = createWriteStream(join(ctx.path, 'countries.json'), { flags: 'w' })
+                        let downloaded = 0
+                        let total = parseInt(res.headers['content-length'], 10)
+                        res.on('data', (chunk) => {
+                            downloaded += chunk.length
+                            task.output = `${(downloaded / total * 100).toFixed(2)}%`
+                            file.write(chunk)
+                        })
+                        res.resume()
+                    })
                 })
             } },
-            { title: 'Load regions', task: async () => {
-                https.get('https://iptv-org.github.io/api/regions.json', res => {
-                    res.pause()
-                    const file = createWriteStream(resolve(__dirname, '../../tmp/data/regions.json'), { flags: 'w' })
-                    res.pipe(file)
-                    res.resume()
+            { title: 'Load regions', task: (ctx, task) => {
+                return new Promise((resolve, reject) => {
+                    https.get('https://iptv-org.github.io/api/regions.json', res => {
+                        res.pause()
+                        res.once('close', resolve)
+                        const file = createWriteStream(join(ctx.path, 'regions.json'), { flags: 'w' })
+                        let downloaded = 0
+                        let total = parseInt(res.headers['content-length'], 10)
+                        res.on('data', (chunk) => {
+                            downloaded += chunk.length
+                            task.output = `${(downloaded / total * 100).toFixed(2)}%`
+                            file.write(chunk)
+                        })
+                        res.resume()
+                    })
                 })
             } },
-            { title: 'Load subdivisions', task: async () => {
-                https.get('https://iptv-org.github.io/api/subdivisions.json', res => {
-                    res.pause()
-                    const file = createWriteStream(resolve(__dirname, '../../tmp/data/subdivisions.json'), { flags: 'w' })
-                    res.pipe(file)
-                    res.resume()
+            { title: 'Load subdivisions', task: (ctx, task) => {
+                return new Promise((resolve, reject) => {
+                    https.get('https://iptv-org.github.io/api/subdivisions.json', res => {
+                        res.pause()
+                        res.once('close', resolve)
+                        const file = createWriteStream(join(ctx.path, 'subdivisions.json'), { flags: 'w' })
+                        let downloaded = 0
+                        let total = parseInt(res.headers['content-length'], 10)
+                        res.on('data', (chunk) => {
+                            downloaded += chunk.length
+                            task.output = `${(downloaded / total * 100).toFixed(2)}%`
+                            file.write(chunk)
+                        })
+                        res.resume()
+                    })
                 })
             } },
-        ], { concurrent: 2 })
+        ], { ...PRESET_TIMER, concurrent: 2 })
     }
 ], { concurrent: false })
 
 async function load() {
-    await listr.run()
+    console.log(resolve(__dirname, '../../tmp/data/channels.json'))
+    await listr.run({
+        path: resolve(__dirname, '../../tmp/data')
+    })
 }
 
 load()
